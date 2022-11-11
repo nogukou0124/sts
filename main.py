89d61ed928e4976ee7264cd5b8672f36d3066190
nogukou0124 <ee207078@meiji.ac.jp>
2022-11-11 23:22:30 +0900

    前処理

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import file
+import pre_pro
 
 learns = file.pre_learn_data()
 tests = file.pre_test_data()
 
-print(tests)
+new_learns = pre_pro.pre_processing(learns)
+new_tests = pre_pro.pre_processing(tests)
+print(new_tests)
